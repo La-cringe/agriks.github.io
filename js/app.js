75addaf6905116971c30392b1706aa9d75d80839
La-cringe <120164741+La-cringe@users.noreply.github.com>
2022-12-09 11:33:06 +0300

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $(".slider").slick({
+    arrows: true,
+    slidesToShow: 1,
+    autoplay: true,
+    speed: 2000,
+    autoplaySpeed: 4000,
+    infinite: true,
+  });
+});
